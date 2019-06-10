ddc0e2e02d1233a2a9d42ae06aa45ad809083d7f
Harry <harrydclark91@gmail.com>
2019-06-10 18:09:49 +0100

    added lines to show track distances

--- folder1_rename/plot_behaviour_summary.py
+++ folder1_rename/plot_behaviour_summary.py
@@ -45,7 +45,6 @@ def plot_stops_in_time(trial_results, session_path):
     beaconed, non_beaconed, probe = split_stop_data_by_trial_type(trial_results)
 
 
-
 def plot_stops_on_track(trial_results, session_path):
     stops_on_track = plt.figure(figsize=(6, 6))
     ax = stops_on_track.add_subplot(1, 1, 1)  # specify (nrows, ncols, axnum)
@@ -53,33 +52,39 @@ def plot_stops_on_track(trial_results, session_path):
     beaconed, non_beaconed, probe = split_stop_data_by_trial_type(trial_results)
 
     for index, _ in beaconed.iterrows():
-        b_stops = np.array(beaconed["stop_locations"][index])
+        b_stops = (np.array(beaconed["stop_locations"][index])*-1)+beaconed["Track Start"][index]
         b_trials = np.array(beaconed["trial_num"][index])*np.ones(len(b_stops))
+
+        ax.plot((np.linspace(beaconed["Track Start"][index], beaconed["Track End"][index], len(b_stops))*-1)+beaconed["Track Start"][index], b_trials, color="y") # marks out track area
         ax.plot(b_stops, b_trials, 'o', color='0.5', markersize=2)
 
     for index, _ in non_beaconed.iterrows():
-        nb_stops = np.array(non_beaconed["stop_locations"][index])
+        nb_stops = (np.array(non_beaconed["stop_locations"][index])*-1)+non_beaconed["Track Start"][index]
         nb_trials = np.array(non_beaconed["trial_num"][index]) * np.ones(len(nb_stops))
+
+        ax.plot((np.linspace(non_beaconed["Track Start"][index], non_beaconed["Track End"][index], len(nb_stops))*-1)+non_beaconed["Track Start"][index], nb_trials, color="y")  # marks out track area
         ax.plot(nb_stops, nb_trials, 'o', color='red', markersize=2)
 
     for index, _ in probe.iterrows():
-        p_stops = np.array(probe["stop_locations"][index])
+        p_stops = (np.array(probe["stop_locations"][index])*-1)+probe["Track Start"][index]
         p_trials = np.array(probe["trial_num"][index]) * np.ones(len(p_stops))
+
+        ax.plot((np.linspace(probe["Track Start"][index], probe["Track End"][index], len(p_stops))*-1)+probe["Track Start"][index], p_trials, color="y")  # marks out track area
         ax.plot(p_stops, p_trials, 'o', color='blue', markersize=2)
 
     plt.ylabel('Stops on trials', fontsize=12, labelpad=10)
     plt.xlabel('Location (vu)', fontsize=12, labelpad=10)
     # plt.xlim(min(spatial_data.position_bins),max(spatial_data.position_bins))
-    plt.xlim(0, 200)
+    #plt.xlim(0, 200)
     ax.yaxis.set_ticks_position('left')
     ax.xaxis.set_ticks_position('bottom')
     #Python_PostSorting.plot_utility.style_track_plot(ax, 200)
     # x_max = max(raw_position_data.trial_number)+0.5
     #Python_PostSorting.plot_utility.style_vr_plot(ax, 200)
     plt.subplots_adjust(hspace=.35, wspace=.35, bottom=0.2, left=0.12, right=0.87, top=0.92)
-    plt.savefig(session_path + '/summary_plot.png', dpi=200)
-    #plt.savefig('/home/harry/aa/plot_summary.png', dpi=200)
-    #plt.show()
+    #plt.savefig(session_path + '/summary_plot.png', dpi=200)
+    plt.savefig('/home/harry/aa/plot_summary.png', dpi=200)
+    plt.show()
     plt.close()
 
 
