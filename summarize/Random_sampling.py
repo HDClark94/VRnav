a1d30ffe755bb68fb011b4bdbe4415b874daba00
majadolny12345 <59828494+majadolny12345@users.noreply.github.com>
2020-01-30 12:11:44 +0000

    Random_sampling code

---
+++ summarize/Random_sampling.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+np.random.randint(low=1, high=100, size=12)
+a = np.random.randint(low=0, high=2, size=100)
+mylist = list(a)
+#now from we converted an array to a list so it can be pasted into excel
